3befc53c115263dff1cecfe48d568bce760fcd3b
Peter Madrid <petermadrid0421@gmail.com>
2024-12-05 14:46:55 +0800

    ui/colortheme

--- lib/screens/lesson/lesson_screen.dart
+++ lib/screens/lesson/lesson_screen.dart
@@ -1,65 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hands/base/res/media.dart';
 import 'package:flutter_hands/base/res/styles/app_styles.dart';
 import 'package:flutter_hands/screens/lesson/widgets/lesson_card.dart';
-import 'package:flutter_hands/base/res/media.dart';
-import 'package:flutter_hands/screens/lesson/modules/Introduction_screen.dart';
-import 'package:flutter_hands/screens/lesson/modules/two_digits_screen.dart';
 import 'package:flutter_hands/screens/lesson/modules/examples_screen.dart';
-
-
+import 'package:flutter_hands/screens/lesson/modules/two_digits_screen.dart';
+import 'package:flutter_hands/screens/lesson/modules/Introduction_screen.dart';
 
 class LessonScreen extends StatelessWidget {
   const LessonScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: ListView(
         children: [
           Container(
+            decoration: BoxDecoration(color: AppStyles.backgroundColor),
+            height: screenHeight,
             padding: const EdgeInsetsDirectional.symmetric(horizontal: 20),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const SizedBox(height: 40),
                 Text("Learn Your Signs", style: AppStyles.headLineStyle1),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 LessonCard(
-                  lessonTitle:"Introduction" ,
-                  lessonSubtitle: "Numbers 0-9", 
+                  lessonTitle: "Introduction",
+                  lessonSubtitle: "Numbers 0-9",
                   lessonIcon: AppMedia.practiceRecognitionBackground,
                   onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const Introduction()),
-            );
-          },
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Introduction()),
+                    );
+                  },
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 LessonCard(
-                  lessonTitle:"Two Digits" ,  
-                  lessonSubtitle: "Numbers 10-99", 
+                  lessonTitle: "Two Digits",
+                  lessonSubtitle: "Numbers 10-99",
                   lessonIcon: AppMedia.practiceRecognitionBackground,
                   onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const TwoDigitsScreen()),
-            );
-          },
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const TwoDigitsScreen()),
+                    );
+                  },
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 LessonCard(
-                  lessonTitle:"Examples" ,  
-                  lessonSubtitle: "Numbers 1-99", 
+                  lessonTitle: "Examples",
+                  lessonSubtitle: "Numbers 1-99",
                   lessonIcon: AppMedia.practiceRecognitionBackground,
                   onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ExamplesScreen()),
-            );
-          },
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ExamplesScreen()),
+                    );
+                  },
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
               ],
             ),
           )
